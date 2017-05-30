8993dd563362f22c631cc5897572db30b9c1eecf
shony <doradopaz@hotmail.com>
2017-05-29 23:46:05 -0600

    fix routes

--- clean_module/routes/route.php
+++ clean_module/routes/route.php
@@ -1,4 +1,4 @@
 <?php
 
 //1XlcpX Module
-Route::get('1XlcpX',   ['as' => 'admin.1XlcpX.index', 'uses' => 'Cms\1XlcpXController@index']);
+Route::get('2XpX',   ['as' => 'admin.2XpX.index', 'uses' => 'Cms\1XlcpXController@index']);
